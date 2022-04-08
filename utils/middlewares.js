8a25455982335e5e60a47b0d4870d20d657f2262
NicoMadd <nicomad.17@gmail.com>
2022-04-08 00:54:54 -0300

    - middleware update removed comments

--- utils/middlewares.js
+++ utils/middlewares.js
@@ -1,32 +1,4 @@
 
-/**
- * @swagger
- *  responses:
- *   #-------------------------------
- *   # Reusable responses (data models)
- *   #-------------------------------
- *    InternalServerError:                                # Can be referenced as '#/responses/InternalServerError'
- *      description: An error has occurred
- *    ForbiddenError:                                     # Can be referenced as '#/responses/ForbiddenError'
- *      description: Error in token, invalid credentials
- *      content:
- *        application/json:
- *          schema:
- *            example: {"error": {"status": 403,"message": "Error! No posee credenciales validas."}}
- *    UnprocessableEntityError:                           # Can be referenced as '#/responses/UnprocessableEntityError'
- *      description: Error, incorrect sintax
- *      content:
- *        application/json:
- *          schema:
- *            example: {  "error": {    "status": 422,    "message": "\"username\" must be a valid email"  }}
- *    UnauthorizedError:                                  # Can be referenced as '#/responses/UnauthorizedError'
- *      description: Error, UnauthorizedError
- *      content:
- *        application/json:
- *          schema:
- *            example: {"error": {"status": 403,"message": "Error! No posee credenciales validas."}}
- *
- */
 const errorsHandler = async (err, req, res, next) => {
 	// logger.error(err);
 	res.status(err.status || 500);
