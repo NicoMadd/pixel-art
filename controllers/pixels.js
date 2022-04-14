b31cf000d606b367bdffb8ed331f1b4266285f00
NicoMadd <nicomad.17@gmail.com>
2022-04-14 12:59:19 -0300

    - api now validates pixel colour change

--- controllers/pixels.js
+++ controllers/pixels.js
@@ -1,68 +1,98 @@
 const { logger } = require("../config/loggerConfig.js")
-const fs = require('fs');
+const fs = require("fs")
 
-const X=10
-const Y=10
+const X = 100
+const Y = 100
+
+const validColours = [
+  "red",
+  "green",
+  "blue",
+  "yellow",
+  "orange",
+  "purple",
+  "pink",
+  "white",
+  "black",
+]
 
 var pixels = []
-var deltaPixels =[]
-var delta = 0;
+var deltaPixels = []
+var delta = 0
 
-fs.readFile('pixels.json', 'utf8', function (err, data) {
-    if (err) throw err;
-    pixels = JSON.parse(data);
-  });
+const validatePixelChange = (pixelId, color) => {
+  if (pixelId < 0 || pixelId >= X * Y) {
+    throw new Error(`PixelId ${pixelId} is out of range`)
+  } else if (validColours.includes(color)) {
+    return
+  } else if (color.length != 6) {
+    throw new Error(`Color ${color} is not a valid color`)
+  } else if (!color.match(/^#[0-9A-F]{6}$/i)) {
+    throw new Error(`Color ${color} is not a valid color`)
+  }
+}
 
-const generateAllPixels =(req, res) => {
-    var id = 1;
-    const color = "#000000";
-    for (var x = 0; x < X; x++) {
-        for(var y = 0; y < Y; y++) {
-            id++
-            var pixel = {id, x, y, color}
-            pixels.push(pixel)
-        }
+fs.readFile("pixels.json", "utf8", function (err, data) {
+  if (err) throw err
+  pixels = JSON.parse(data)
+})
+
+const generateAllPixels = (req, res) => {
+  var id = 1
+  const color = "#000000"
+  for (var x = 0; x < X; x++) {
+    for (var y = 0; y < Y; y++) {
+      id++
+      var pixel = { id, x, y, color }
+      pixels.push(pixel)
     }
-    fs.writeFileSync('pixels.json', JSON.stringify(pixels));
+  }
+  fs.writeFileSync("pixels.json", JSON.stringify(pixels))
 }
 
 const getAllPixels = async (req, res) => {
-    logger.info("getAllPixels")
-    res.send({pixels,delta})
+  logger.info("getAllPixels")
+  res.send({ pixels, delta })
 }
 
 const setPixel = async (req, res) => {
-	const {pixelId, color} = req.body
+  try {
+    const { pixelId, color } = req.body
     console.log(pixelId, color)
-    const pixel = pixels.find(pixel => pixel.id == pixelId)
+    validatePixelChange(pixelId, color)
+    const pixel = pixels.find((pixel) => pixel.id == pixelId)
+    if (!pixel) throw new Error(`PixelId ${pixelId} doesnt exist`)
     pixel.color = color
     deltaPixels.push(pixel)
     delta++
-    res.send({delta})
-    
+    res.send({ delta })
+  } catch (error) {
+    logger.error(error)
+    res.status(409).send({ error: { message: error.message } })
+  }
 }
 
 const getDeltaPixels = async (req, res) => {
-    // logger.info("getDeltaPixels")
-    const { deltaID } = req.params
-    // console.log("incoming delta",deltaID)
-    // console.log("delta",delta)
-    if(delta != deltaID) {
-        newPixels = deltaPixels.slice(deltaID, delta)
-        res.send({delta,pixels:newPixels})
-    }else{
-        res.send({delta})
-    }
+  // logger.info("getDeltaPixels")
+  const { deltaID } = req.params
+  // console.log("incoming delta",deltaID)
+  // console.log("delta",delta)
+  if (delta > deltaID) {
+    newPixels = deltaPixels.slice(deltaID, delta)
+    res.send({ delta, pixels: newPixels })
+  } else {
+    res.send({ delta, pixels: [] })
+  }
 }
 
 const getDelta = async (req, res) => {
-    // logger.info("getDelta")
-    res.send({delta})
+  // logger.info("getDelta")
+  res.send({ delta })
 }
 
 module.exports = {
-getAllPixels,
-setPixel,
-getDeltaPixels,
-getDelta
+  getAllPixels,
+  setPixel,
+  getDeltaPixels,
+  getDelta,
 }
